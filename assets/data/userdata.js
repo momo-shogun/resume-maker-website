e45a91ae6bc4571e382ad71840158e5d4024aa4c
momo-shogun <johndoe@example.com>
2024-01-08 09:39:50 +0530

    education , certification thrugh form

--- assets/data/userdata.js
+++ assets/data/userdata.js
@@ -1,19 +1,7 @@
 export let userInformation = JSON.parse(localStorage.getItem('userInformation'));
 
 
-export let education = [{
-    name: 'High School, Lucknow Public School, Lucknow.',
-    year: '2018–2019',
-    percent: '95%'
-}, {
-    name: 'Intermediate, DSB International Public School, Rishikesh.',
-    year: '2020–2021',
-    percent: '85%'
-}, {
-    name: 'BCA, Babasaheb Bhimrao Ambedkar University, Lucknow',
-    year: '2021-Currently',
-    percent: '7.9 CGPA'
-}];
+export let education = JSON.parse(localStorage.getItem('education')) || [];
 
 export let skills = JSON.parse(localStorage.getItem('skills')) || [];
 
@@ -32,22 +20,20 @@ export let projects = [
         madeWith: 'HTML, CSS, JavaScript, Next.js, Node.js, and EJS.'
     }
 ];
-export let certification = [
-    'Certification 1: Certification Name 1',
-    'Certification 1: Certification Name 1'
-];
 
-export let aboutMe = [
-    'Driven by a fervent passion for Competitive Programming (CP)',
-    'Dedicated to the art of web development, I find joy in the process of transforming ideas into functional and aesthetically pleasing websites.',
-    'Love exploring different types of music to find comfort and inspiration.',
-    'Avid follower of anime culture, appreciating storytelling, character development, and diverse genres within the medium.'
-];
+export let certification = JSON.parse(localStorage.getItem('certification')) || [];
+
+
+
+export let aboutMe = JSON.parse(localStorage.getItem('aboutMe')) || [];
 
 
 export function updateUserInformation() {
-    localStorage.removeItem('skills');
+    localStorage.removeItem('skills','education','certificate','aboutMe');
     skills = [];
+    education = [];
+    certification = [];
+    aboutMe = [];
 
     userInformation.name = document.getElementById('name').value || 'Albert Gomega';
     userInformation.phone = document.getElementById('phone').value || 1234567890;
@@ -79,21 +65,49 @@ export function updateUserInformation() {
     //education
     let schoolName = document.querySelectorAll('.js-school-name');
     let schoolStart = document.querySelectorAll('.js-school-start');
-    let schoolStart = document.querySelectorAll('.js-school-start');
+    let schoolEnd = document.querySelectorAll('.js-school-end');
+    let schoolGrade = document.querySelectorAll('.js-school-grade');
 
+    for (let i = 0; i < schoolName.length; i++) {
+        let tempschoolName = schoolName[i].value;
+        let tempschoolStart = schoolStart[i].value;
+        let tempschoolEnd = schoolEnd[i].value;
+        let tempschoolGrade = schoolGrade[i].value;
 
-    for (let i = 0; i < inputTitles.length; i++) {
-        let skillName = inputTitles[i].value;
-        let skillDescription = inputDescriptions[i].value;
-        if (skillName) {
+        if (tempschoolName) {
             let temp = {
-                name: skillName,
-                description: skillDescription
-
+                name: tempschoolName,
+                year: `${tempschoolStart} to ${tempschoolEnd}`,
+                percent:tempschoolGrade
             }
-            skills.push(temp);
+            education.push(temp);
         }
     }
-    localStorage.setItem('skills', JSON.stringify(skills));
+    localStorage.setItem('education', JSON.stringify(education));
 
+    //Projects 
+
+    //Certifications
+    let certificate = document.querySelectorAll('.js-certificate');
+
+    for (let i = 0; i < certificate.length; i++) {
+        let tempCertificate = certificate[i].value;
+        console.log(tempCertificate);
+        if (tempCertificate) {
+            certification.push(tempCertificate);
+        }
+    }
+    localStorage.setItem('certification', JSON.stringify(certification));
+
+    //Interests
+    let interest = document.querySelectorAll('.js-interest');
+
+    for (let i = 0; i < interest.length; i++) {
+        let tempinterest = interest[i].value;
+        console.log(tempinterest);
+        if (tempinterest) {
+            aboutMe.push(tempinterest);
+        }
+    }
+    localStorage.setItem('aboutMe', JSON.stringify(aboutMe));
 }
